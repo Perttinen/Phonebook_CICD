7ffb26e6309c0bd8442af51096cb2d26d00a3154
perttinen <samuli.viitala@gmail.com>
2023-09-01 10:23:39 +0300

    Fixed some lint warnings

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
 const errorHandler = (error, req, res, next) => {
-  console.error(error.message, error.name, error.extra)
+  console.error(error.message, error.name, error.extra) //eslint-disable-line
 
   if (error.name === 'ApplicationError') {
     return res.status(error.status).send({ error: error.message })
